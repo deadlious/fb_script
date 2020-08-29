ea37fc93a40f78e12e1be5ec72ee111607fc9327
deadlious <48437032+deadlious@users.noreply.github.com>
2020-08-29 15:13:51 +0300

    Update fb.js

--- fb.js
+++ fb.js
@@ -1,6 +1,55 @@
+function sort_groups(){
+	// Switch to chronological order the posts in groups.
+	let count = 0;
+	let gr_links = document.querySelectorAll('a[href^="https://www.facebook.com/groups"],' + 'a[href^="/groups/"]');
+	for(var i=0; i<gr_links.length; i++){
+		
+		cls = gr_links[i].getAttribute("class");
+		if(cls.indexOf("scd") < 0){
+			gr_links[i].setAttribute("class", cls + " scd");
+			remove_listeners(gr_links[i]);
+		}
+		
+		let gr_href = gr_links[i].getAttribute('href');
+		if (gr_href.indexOf('permalink') < 0 && gr_href.indexOf('CHRONOLOGICAL') < 0){
+			let q = gr_href.indexOf('?');
+			q = q==-1?q:q-1;
+			gr_href = gr_href.slice(0, q) + '?sorting_setting=CHRONOLOGICAL';
+			gr_links[i].setAttribute('href', gr_href);
+			count += 1;
+		}
+	}
+	if(count > 0) console.log("scd replaced group links: " + count);
+}
+
+function remove_listeners(elem){
+	elem.addEventListener("mousedown", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("mouseup", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("click", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("mouseover", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("mouseenter", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("mouseleave", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("mousemove", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("mouseout", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("focus", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("focusin", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("focusout", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("visibilitychange", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("pointerdown", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("pointerup", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("gotpointercapture", function (event) {event.stopPropagation();}, true);
+	elem.addEventListener("pointerover", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("load", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("auxclick", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("pointermove", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("pointerout", function (event) { event.stopPropagation();}, true);
+	elem.addEventListener("DOMContentLoaded", function (event) { event.stopPropagation();}, true);
+}
+
 function redirect_plus(){
 // Redirect to "Recent Posts" view of news feed
 	let wl_href = window.location.href;
+	let count = 0;
 	
 	if (wl_href == "https://www.facebook.com/" ||
 		// window.location.href == "https://www.facebook.com/?sk=h_nor" ||
@@ -9,24 +58,23 @@ function redirect_plus(){
 	   window.location.href = 'https://www.facebook.com/?sk=h_chr'; 
 	}
 
-// Switch to chronological order the posts in groups.
-	if (wl_href.indexOf('groups') > 0 && 
-		wl_href.indexOf('notif_id') < 0 &&
-		wl_href.indexOf('CHRONOLOGICAL') < 0){
-			wl_href = wl_href.slice(0, wl_href.indexOf('?')) + 
-						'?sorting_setting=CHRONOLOGICAL';
-			window.location.href = wl_href;
-		}
+	sort_groups();
 	
 // Change links to point recent news feed.
-	var count = 0;
+	count = 0;
 	var links = document.querySelectorAll(
 		'a[href^="https://www.facebook.com/?ref="], ' +
 		'a[href="https://www.facebook.com/"], ' +
-		'a[href^="/?sk="] ' 
+		'a[href^="/?sk="], ' + 'a[href="/"] ' 
 		// + ', a[href^="https://www.facebook.com/?sk="] '
 		);
 	for (var a = 0; a<links.length; a++){
+		
+		cls = links[a].getAttribute("class");
+		if(cls.indexOf("scd") < 0){
+			links[a].setAttribute("class", cls + " scd");
+			remove_listeners(links[a]);
+		}
 		
 		if (links[a] && 
 			links[a].getAttribute('href') !== 'https://www.facebook.com/?sk=h_chr' ) {
@@ -34,43 +82,48 @@ function redirect_plus(){
 			count +=1;
 		} 
 	}
-	if(count > 0) console.log("EVB replaced noob links: " + count);
+	if(count > 0) console.log("scd replaced noob links: " + count);
 	
-	switchToHrono()
+	switchToHrono();
 }
+
+
 
 function switchToHrono() {
 // Change links to strip facebook tracking
+	
 	let count = 0;
 	var ahref = document.querySelectorAll('a[href*="fbclid"]');
 	// console.log('number of a links: ' + ahref.length);  
 	for(var i = 0; i < ahref.length; i++){
+		cls = ahref[i].getAttribute("class");
+		if(cls.indexOf("scd") < 0){
+			ahref[i].setAttribute("class", cls + " scd");
+			remove_listeners(ahref[i]);
+		}
+		
 		var href = ahref[i].getAttribute('href');
-		if (href !== null){
-			//console.log('evb original fb url: ' + href);
-			if (href.indexOf('https://l.facebook.com/l.php?u=') !== -1){
-					href = href.replace('https://l.facebook.com/l.php?u=','');
-					//console.log('evb sliced url: ' + href);
-					href = decodeURIComponent( href );
-			}
-			var ind = href.indexOf('fbclid');
-			if ( ind !== -1){
-				ind = ind - 1;
-				href = href.slice( 0, ind );
-				//href = href.slice( 0, href.indexOf('&fbclid') );
-				
-				//console.log('evb decoded url: ' + href);
-				ahref[i].setAttribute('href', href);
-				// console.log('evb replaced with: ' + href);
-				count +=1;
-			}
-			if( ahref[i].getAttribute('data-lynx-uri') ){
-				ahref[i].setAttribute('data-lynx-uri', href);
-			}
+		href = href.replace('https://l.facebook.com/l.php?u=','');
+		//console.log('scd sliced url: ' + href);
+		href = decodeURIComponent( href );
+		var ind = href.indexOf('fbclid');
+		if ( ind !== -1){
+			ind = ind - 1;
+			href = href.slice( 0, ind );
+			//href = href.slice( 0, href.indexOf('&fbclid') );
+			
+			//console.log('scd decoded url: ' + href);
+			ahref[i].setAttribute('href', href);
+			// console.log('scd replaced with: ' + href);
+			count +=1;
+		}
+		if( ahref[i].getAttribute('data-lynx-uri') ){
+			ahref[i].setAttribute('data-lynx-uri', href);
 		}
 	}
-	if(count > 0) console.log('EVB Converted urls: ' + count);
+	if(count > 0) console.log('scd Converted urls: ' + count);
 	
+	sort_groups();
 	
 // Hide post adds
 	var par = null;
@@ -83,11 +136,12 @@ function switchToHrono() {
 	selector += ', div.x_1fo12d-fii > div.i_1fo12d-nuz > div._6a > a.x_1fo12d-mtm';
 	//selector += ', div.clearfix > div._-ix._ohf > div._6a > a._42ft[role=button]';
 	// selector += ', a._42ft._4jy0._517h._51sy:not(._55pi):not(._522u)[role=button]';
+	selector += ', div.qzhwtbm6 > span.oi732d6d > span.jsc_c_3e > div.oajrlxb2[role="button"]';
 	adds = document.querySelectorAll(selector);
 	count = 0;
 	
 	for(var j = 0; j < adds.length; j++){
-		if(adds[j].id !== null && adds[j].id.slice(0,3) !== 'evb'){
+		if(adds[j].id !== null && adds[j].id.slice(0,3) !== 'scd'){
 			par = adds[j].parentElement;
 			feed = true;
 			do {
@@ -95,8 +149,8 @@ function switchToHrono() {
 					// par.style = "display: none;";
 					var add_title = par.querySelector('h5 > span > span > a');
 					if(add_title!==null){
-						par.innerHTML = 'EVB: HIDDEN_ADD ( from: ' + add_title.outerHTML + ')' ;
-						adds[j].id = 'evb_add' + j;
+						par.innerHTML = 'scd: HIDDEN_ADD ( from: ' + add_title.outerHTML + ')' ;
+						adds[j].id = 'scd_add' + j;
 						count += 1;
 					}
 					feed = false;
@@ -108,7 +162,7 @@ function switchToHrono() {
 			
 		}
 	}
-	if(count > 0) console.log('EVB Hidden Post Adds: ' + count);
+	if(count > 0) console.log('scd Hidden Post Adds: ' + count);
 	
 	// Hide side adds
 	adds = document.querySelectorAll('div > a[rel="nofollow"]');
@@ -117,13 +171,13 @@ function switchToHrono() {
 	feed = null;
 	
 	for( j = 0; j < adds.length; j++){
-		if(adds[j].id !== null && adds[j].id.slice(0,3) !== 'evb'){
+		if(adds[j].id !== null && adds[j].id.slice(0,3) !== 'scd'){
 			par = adds[j].parentElement;
 			feed = true;
 			do {
 				if(par.id.indexOf('pagelet_ego_pane') !== -1){
 					par.style = "display: none;";
-					adds[j].id = 'evb_add' + j;
+					adds[j].id = 'scd_add' + j;
 					count += 1;
 					feed = false;
 				} else{
@@ -133,7 +187,7 @@ function switchToHrono() {
 			
 		}
 	}
-	if(count > 0) console.log('EVB Hidden Adds: ' + count);
+	if(count > 0) console.log('scd Hidden Adds: ' + count);
 
 // Pause all videos when scrolling
 	count = 0;
@@ -144,11 +198,12 @@ function switchToHrono() {
 	    	count += 1;
 		}
 	}
-	if(count > 0) console.log('EVB paused watch parties: ' + count);
+	if(count > 0) console.log('scd paused watch parties: ' + count);
 }
 
+
 // Set scrolling event to the global Container
-document.getElementById("globalContainer").addEventListener("wheel", switchToHrono);
+document.getElementById("mount_0_0").addEventListener("wheel", redirect_plus);
 
 // Run once, once loaded
 window.setTimeout(redirect_plus, 500);
